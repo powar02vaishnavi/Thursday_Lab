c0861be7bb99ab7e960c595af3b2dae6185132c8
Vaishnavi Powar <59617469+powar02vaishnavi@users.noreply.github.com>
2022-09-29 21:02:47 +0530

    Create HCFofTwo.java

---
+++ 29-09-2022/HCFofTwo.java
@@ -0,0 +1,30 @@
+/*
+Lab QUE2:Write a program to calculate HCF of Two given number.
+
+*/
+
+import java.util.*;
+public class HCFofTwo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n1, n2, hcf=0;
+		Scanner sc = new Scanner(System.in);
+		//Taking input from user for 1st number
+		System.out.println("Enter the First Number");
+		n1=sc.nextInt();
+		//Taking input from user for 2nd number
+		System.out.println("Enter the Second Number");
+		n2=sc.nextInt();
+		//using for loop to HCF [Highest Common Factor] we initialize i in this loop
+	    for (int i=1;i<=n1||i<=n2;i++)
+	      {
+	    	//checking if both numbers are completely divided by i with if conditional statement
+	     if (n1%i==0&&n2%i==0)
+	        hcf=i;
+	      }
+	    //Display the HCF
+	    System.out.println("The HCF of Two Given Numbers is : "+hcf);
+	}
+
+}
