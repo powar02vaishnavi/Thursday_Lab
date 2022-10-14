1697f5eb29aba177286e3e3474e9f37e272581d8
Vaishnavi Powar <59617469+powar02vaishnavi@users.noreply.github.com>
2022-10-14 17:54:29 +0530

    Create run.java

---
+++ 13-10-2022/multiLevelInheritance/run.java
@@ -0,0 +1,13 @@
+package multiLevelInheritance;
+
+//WAP to create a class for Reverse No, second class Sum of Digit, Third class use both the class property and display the output
+public class run extends sumdig {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Calling rev and sumdigit function from inherited Classes
+		rev();
+		sumdigit();
+	}
+
+}
