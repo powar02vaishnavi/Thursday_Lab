5ec2f4a4267e5bd408da2cdf4e4de40d33858746
Vaishnavi Powar <59617469+powar02vaishnavi@users.noreply.github.com>
2022-10-14 17:56:36 +0530

    Create run.java

---
+++ 13-10-2022/palindrome/run.java
@@ -0,0 +1,10 @@
+//WAP to create a class which contain the method Palindrome, the second class will use the proerty of first class and display the output.
+public class run extends palindromeOrNot {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//calling palindrome function from inherited class
+		palindrome();
+	}
+
+}
