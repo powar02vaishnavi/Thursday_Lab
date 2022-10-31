06fdea335bf959dabce01872dc32588860d92b0b
Vaishnavi Powar <59617469+powar02vaishnavi@users.noreply.github.com>
2022-10-31 17:19:33 +0530

    Create employeeSalaryExpenses.java

---
+++ 21-10-2022/employeeSalaryExpenses/employeeSalaryExpenses.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class employeeSalaryExpenses {
+	String name,address;
+	   int e_id,salary,exp; 
+	   long phone_no;
+	   static int max_sal;
+	   Scanner sc=new Scanner(System.in);
+	   public int getSalary() 
+	   {
+		return salary;
+	   }
+	   public void setSalary(int salary)
+	   {
+		   this.salary = salary;
+	   }
+	 
+	}
